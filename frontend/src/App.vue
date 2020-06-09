cff9f8c52696bb709aaeb0d92e7ee0783d46d962
ClodoaldoDantas <clodoaldodantas8@gmail.com>
2020-06-09 20:41:38 -0300

    tela inicial

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,62 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+html {
+  font-size: 62.5%;
+  box-sizing: inherit;
 }
 
-#nav a {
+:root {
+  --color-grey: #555;
+  --color-grey-medium: #666;
+  --color-grey-light: #fafafa;
+  --color-blue: #3498db;
+}
+
+body {
+  font-size: 1.6rem;
+  font-family: "Open Sans", sans-serif;
+  color: var(--color-grey);
+}
+
+h1,
+h2,
+h3 {
+  font-family: "Ubuntu", sans-serif;
   font-weight: bold;
-  color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.container {
+  max-width: 120rem;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+}
+
+.btn {
+  font-family: "Open Sans", sans-serif;
+  font-size: 1.6rem;
+  border: none;
+  border-radius: 1.5rem;
+  text-transform: uppercase;
+  background: var(--color-blue);
+  color: #fff;
+  padding: 1rem 5rem;
+  cursor: pointer;
+  outline: 0;
+  transition: opacity 200ms ease;
+}
+
+.btn:hover {
+  opacity: 0.8;
 }
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,92 @@
 <template>
-  <div class="home">
-    <h1>Hello World</h1>
-  </div>
+  <section class="home">
+    <div class="container">
+      <div class="home-content">
+        <h1>DevFinder</h1>
+        <p>
+          precisa de um desenvolvedor perto de <br />
+          você ? Encontre-o aqui.
+        </p>
+        <button class="btn btn-home">
+          <span>inicie agora mesmo</span>
+          <log-in-icon size="24"></log-in-icon>
+        </button>
+      </div>
+      <figure class="home-hero">
+        <img src="@/assets/hero.svg" alt="hero" />
+      </figure>
+    </div>
+  </section>
 </template>
 
 <script>
+import { LogInIcon } from "vue-feather-icons";
+
 export default {
   name: "Home",
+  components: {
+    LogInIcon,
+  },
 };
 </script>
+
+<style scoped>
+.home {
+  background: var(--color-grey-light);
+  height: 100vh;
+  width: 100%;
+}
+
+.home > .container {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.home-hero img {
+  width: 60rem;
+  height: auto;
+}
+
+.home-content h1 {
+  color: var(--color-blue);
+  font-size: 4.8rem;
+  margin-bottom: 1rem;
+}
+
+.home-content p {
+  font-size: 2rem;
+  color: var(--color-grey-medium);
+  margin-bottom: 2rem;
+}
+
+.btn-home {
+  display: flex;
+  align-items: center;
+}
+
+.btn-home span {
+  margin-right: 1rem;
+}
+
+@media (max-width: 960px) {
+  .home-hero img {
+    width: 50rem;
+    height: auto;
+  }
+}
+
+@media (max-width: 768px) {
+  .home > .container {
+    flex-direction: column;
+  }
+
+  .home-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+  }
+}
+</style>
